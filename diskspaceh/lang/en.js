2394d6fc8d34655381ff98e7c13a72dbc1f7c49b
JMSolo <jmsolodesigns@gmail.com>
2016-07-05 08:11:13 -0500

    Revert "[fix] allow diskspaceh plugin to be read properly by rutorrent"
    This reverts commit e1605f8e0a51f9609af4d495b4c9751e3aa1657c.

--- diskspaceh/lang/en.js
+++ diskspaceh/lang/en.js
@@ -3,9 +3,9 @@
  *
  * English language file.
  *
- * Author:
+ * Author: 
  */
 
  theUILang.diskNotification = "Warning! The disk is full. rTorrent may not run correctly, and no data will be downloaded until you free some disk space.";
 
-thePlugins.get("diskspaceh").langLoaded();
+thePlugins.get("diskspace").langLoaded();
